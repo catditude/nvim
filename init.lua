52567fb9c65acd958b31289b4e0b95e6b9d0f642
Di <dihuynh2302@gmail.com>
2026-02-23 10:52:11 -0500

    feat: highlight references on CursorHold

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.scrolloff = 999
+vim.opt.updatetime = 300
 vim.cmd.colorscheme("1989")
 
 require("lazy_init")
--- lua/lsp_setup.lua
+++ lua/lsp_setup.lua
@@ -12,3 +12,22 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client:supports_method('textDocument/documentHighlight') then
+      local group = vim.api.nvim_create_augroup('lsp_document_highlight_' .. args.buf, { clear = true })
+      vim.api.nvim_create_autocmd('CursorHold', {
+        group = group,
+        buffer = args.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+      vim.api.nvim_create_autocmd('CursorMoved', {
+        group = group,
+        buffer = args.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+  end,
+})
+
