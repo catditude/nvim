0aea9f9390d9cac040e7ca4e0b12b8473bac7246
Di <dihuynh2302@gmail.com>
2026-03-03 22:36:56 -0500

    fix: hide default tabline in diffview to avoid duplicate file path lines
    Add view_opened/view_closed hooks to toggle showtabline, and enable
    enhanced_diff_hl and winbar_info for both default and file_history views.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -7,10 +7,22 @@ return {
     { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File history" },
   },
   opts = {
+    enhanced_diff_hl = true,
+    view = {
+      default = { winbar_info = true },
+      file_history = { winbar_info = true },
+    },
     hooks = {
       diff_buf_read = function(bufnr)
         -- Trigger BufReadPost so treesitter-context recognizes diff buffers
         vim.api.nvim_exec_autocmds("BufReadPost", { buffer = bufnr })
+      end,
+      view_opened = function()
+        -- Hide the default tabline (shows file paths when diffview creates a new tab)
+        vim.opt.showtabline = 0
+      end,
+      view_closed = function()
+        vim.opt.showtabline = 1
       end,
     },
     keymaps = {
