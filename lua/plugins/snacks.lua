ddd07c112b9d840ebd8e7f4b8292439227d8793d
Di <dihuynh2302@gmail.com>
2026-03-15 11:01:41 -0400

    add icon for quit

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -34,7 +34,7 @@ return {
                     { icon = "󰊄 ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                     { icon = "󰋚 ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
                     { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
                 },
             },
             sections = {
